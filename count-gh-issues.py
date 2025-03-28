cf4a6e885b4a7781f5f5e7a4a4c648a83b520e9e
Alex Dewar <a.dewar@imperial.ac.uk>
2025-03-28 11:20:49 +0000

    Exclude issues from archived repos + myriad linter fixes

--- count-gh-issues.py
+++ count-gh-issues.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+"""A script to get a count of open GitHub issues."""
 
 import json
 import subprocess as sp
@@ -8,15 +9,19 @@ from datetime import datetime
 import dateparser
 
 
-def main(user: str, from_date_str: str):
+def main(user: str, from_date_str: str) -> None:
+    """Run the script."""
     from_date = dateparser.parse(from_date_str)
-    cmd = f"gh search issues --assignee {user} --state open -L 1000 --json updatedAt"
+    cmd = (
+        f"gh search issues --assignee {user} --archived=false --state open "
+        "-L 1000 --json updatedAt"
+    )
 
     ret = sp.run(cmd.split(" "), capture_output=True)
     dates = (datetime.fromisoformat(val["updatedAt"]) for val in json.loads(ret.stdout))
-    count = len(list(date > from_date for date in dates))
+    count = len(list(date > from_date for date in dates))  # type: ignore[operator]
     print(count)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
